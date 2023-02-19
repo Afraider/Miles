a8e9d3806bfeaecc9b867dd5c8ff92a5e5b197e8
Ivan <fojzzzz@gmail.com>
2023-02-19 10:46:56 +0300

    add Main

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1450;
+        int miles = (ticketPrice / 20);
+        System.out.println("Благодарим за покупку! Вам будут начислены бонусные " + miles + " мили");
+    }
+}
