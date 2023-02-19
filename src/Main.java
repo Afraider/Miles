c449ff66714d2629f02a0c746f64bdae7459537e
Ivan <fojzzzz@gmail.com>
2023-02-19 10:57:13 +0300

    add Mai

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 1450;
-        int miles = (ticketPrice / 20);
-        System.out.println("Благодарим за покупку! Вам будут начислены бонусные " + miles + " мили");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
